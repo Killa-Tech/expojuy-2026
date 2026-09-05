6b92dbaa95d6d8db50369d7faa5a05780aff7e86
jesus-rodriguez <jesus.dev.rodriguez@gmail.com>
2026-09-04 22:24:23 -0300

    feat(main): use floating menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ThemeProvider } from './contexts/theme'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./contexts/theme";
+import { FloatingMenuProvider } from "./contexts/floating-menu/provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <FloatingMenuProvider>
+        <App />
+      </FloatingMenuProvider>
     </ThemeProvider>
   </StrictMode>,
-)
+);
